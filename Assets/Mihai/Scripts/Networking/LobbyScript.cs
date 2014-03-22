4a5941fd154fd005280dacbb61c333fdc93790a6
mihai9323 <mihai9323@gmail.com>
2014-03-22 03:13:31 +0100

    Added some lobby network features
    Need test

---
+++ Assets/Mihai/Scripts/Networking/LobbyScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyScript : MonoBehaviour {
+	HostData[] Hd;
+	NetworkInterface NI;
+	void Awake () 
+	{
+		NI = GameObject.Find ("NetworkInterface").GetComponent<NetworkInterface> ();
+	}
+
+	void CreateServer()
+	{
+		NI.CreateServer();
+	}
+
+	HostData[] GetServerList()
+	{
+		return NI.GetServerList();
+
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	void OnGUI()
+	{
+		if (Hd != null) {
+					for (int i = 0; i < Hd.Length; i++) {
+						Debug.Log ((Hd [i].gameName + " " + Hd [i].connectedPlayers + "/" + Hd [i].playerLimit + " players").ToString ());
+			
+						if (GUI.Button (new Rect (255, 20 + 53 * i, 400, 50), (Hd [i].gameName + " " + Hd [i].connectedPlayers + "/" + Hd [i].playerLimit + " players").ToString ())) {
+							NI.Connect(Hd[i]);
+						}
+					}
+					Debug.Log ("hostdata length: " + Hd.Length);
+				}
+
+		if(Network.isServer || Network.isClient)
+		{
+			if(GUI.Button( new Rect(50, 20, 200, 50), "Get Server List"))
+			{
+				Hd =GetServerList();
+
+
+
+			}
+		}
+		else 
+		{
+
+			if(GUI.Button( new Rect(50, 20, 200, 50), "Create Server"))
+			{
+				CreateServer();
+			}
+		}
+	}
+}
---
+++ Assets/Mihai/Scripts/Networking/NetworkInterface.cs
@@ -0,0 +1,321 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkInterface : MonoBehaviour {
+	static GameObject _instance;
+	bool hasWon = false;
+
+	string gameName = "GLGameJam14.TrickTheMage";
+	string gameTypeName = "GLGameJam14.TrickTheMage";
+	bool isRefreshingServerList = false;
+	HostData[] hostdata;
+
+	void Awake()
+	{
+		//Util.Settings.DrawLog = false;
+		if(_instance != null)
+		{
+			_instance.GetComponent<NetworkInterface>().CleanUp ();
+			GameObject.Destroy(_instance);
+		}
+		_instance = gameObject;
+	}
+
+	void Start()
+	{
+		gameName += SystemInfo.deviceName.ToString();
+		InvokeRepeating("RefreshHelper", 0f, 1f);
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void RefreshHelper()
+	{
+		StartCoroutine("RefreshServerList");
+	}
+
+	public void CreateServer()
+	{
+		Network.InitializeServer(2, 25002, true);
+		MasterServer.RegisterHost(gameTypeName, gameName);
+	}
+
+	public void CreateServerWithName(string namerzorz)
+	{
+		Network.InitializeServer(2, 25002, true);
+		MasterServer.RegisterHost(gameTypeName, namerzorz);
+	}
+
+	public void Connect(string guid)
+	{
+		Network.Connect(guid);
+
+	}
+	public void Connect(HostData hd)
+	{
+		Network.Connect (hd);
+		
+	}
+
+	public HostData[] GetServerList()
+	{
+		return hostdata;
+	}
+
+	private IEnumerator RefreshServerList()
+	{
+//		if(Network.isServer || Network.isClient)
+//			yield return null;
+		MasterServer.RequestHostList(gameTypeName);
+		yield return new WaitForSeconds(2f);
+		hostdata = MasterServer.PollHostList();
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server has been initialized");
+	}
+
+	void OnApplicationQuit()
+	{
+		Network.Disconnect(0);
+		MasterServer.UnregisterHost();
+	}
+
+	void OnConnectedToServer()
+	{
+		Debug.Log("Connected to server");
+	}
+
+	void OnDisconnectedFromServer()
+	{
+		Debug.Log("Disconnected from server");
+	}
+
+	void OnFailedToConnect()
+	{
+		Debug.Log("Failed to connect");
+	}
+
+	void OnPlayerConnected()
+	{
+		Debug.Log("Player connected");
+		oStartGame("TubeOne");
+	}
+
+	void OnPlayerDisconnected()
+	{
+		Debug.Log("Player disconnected");
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent mse)
+	{
+		if(mse == MasterServerEvent.RegistrationSucceeded)
+			Debug.Log("OnMasterServerEvent: Registration of server successful");
+		else if(mse == MasterServerEvent.RegistrationFailedGameName)
+			Debug.Log("OnMasterServerEvent: Registration of server failed: game name");
+		else if(mse == MasterServerEvent.RegistrationFailedNoServer)
+			Debug.Log("OnMasterServerEvent: Registration of server failed: no server");
+		else if(mse == MasterServerEvent.RegistrationFailedGameType)
+			Debug.Log("OnMasterServerEvent: Registration of server failed: game type");
+		else if(mse == MasterServerEvent.HostListReceived)
+			Debug.Log("OnMasterServerEvent: Host list received");
+	}
+
+//	void OnGUI()
+//	{
+//		if(Application.loadedLevelName == "Lobby")
+//		{
+//			if(GUI.Button(new Rect(0,Screen.height-30, 100, 30), "Ping"))
+//			{
+//				networkView.RPC("Ping", RPCMode.Others, null);
+//			}
+//
+//			if(GUI.Button(new Rect(25, 65, 150, 30), "Refresh Server List"))
+//			{
+//				StartCoroutine("RefreshServerList");
+//			}
+//
+//			if(hostdata != null)
+//			{
+//				for(int i = 0; i < hostdata.Length; i++)
+//				{
+//					if(GUI.Button(new Rect(200, 25 + 40 * i, 300, 30), hostdata[i].gameName.ToString()))
+//						ConnectToServer(hostdata[i]);
+//				}
+//			}
+//
+//			if(Network.isServer)
+//				return;
+//			
+//			if(GUI.Button(new Rect(25, 25, 150, 30), "Start New Server"))
+//			{
+//				CreateServer();
+//			}
+//		}
+//	}
+
+	void ConnectToServer(HostData hd)
+	{
+		Debug.Log("Connecting to server...");
+		Network.Connect(hd.guid);
+	}
+
+	#region rpc's
+//	[RPC]
+//	void Ping()
+//	{
+//		GameObject.Instantiate(networkCube, Vector3.zero, Quaternion.identity);
+//	}
+
+	[RPC]	
+	void iSendProgress(float distance)
+	{
+		//GameObject.Find("GameController").GetComponent<GameController>().distanceRunOther = distance;
+	}
+
+	public void oSendProgress(float distance)
+	{
+		networkView.RPC("iSendProgress", RPCMode.Others, new object[]{distance});
+	}
+
+	[RPC]	
+	void iHitFirewall()
+	{
+		Debug.Log("iHitFirewall not implemented yet");
+	}
+
+	public void oHitFirewall()
+	{
+		networkView.RPC("iHitFirewall", RPCMode.Others, null);
+	}
+
+	[RPC]	
+	void iVibrate(float duration)
+	{
+		Debug.Log("iVibrate not implemented yet");
+	}
+
+	public void oVibrate()
+	{
+		Debug.Log("oVibrate not implemented yet");
+		networkView.RPC("iVibrate", RPCMode.Others, new object[]{1f});
+	}
+
+	[RPC]	
+	void iGlitch()
+	{
+		//GameObject.Find("GameController").GetComponent<GameController>().SetPopUp();
+	}
+
+	public void oGlitch()
+	{
+		Debug.Log("oGlitch not implemented yet");
+		networkView.RPC("iGlitch", RPCMode.Others, null);
+	}
+
+	[RPC]	
+	void iMute()
+	{
+		//AndroidControls.Mute();
+	}
+
+	public void oMute()
+	{
+		networkView.RPC("iMute", RPCMode.Others, null);
+	}
+
+	[RPC]	
+	void iSetBrightness()
+	{
+		//GameObject.Find("Center").GetComponent<AvatarActions>().Brightness();
+	}
+
+	public void oSetBrightness()
+	{
+		networkView.RPC("iSetBrightness", RPCMode.Others, null);
+	}
+
+	bool haveFinished = false;
+	public bool iWon = false; 
+	[RPC]	
+	void iFinished(float time)
+	{
+	//	if(!haveFinished)
+			//GameObject.Find("GameController").GetComponent<GameController>().FinishScreen(false, time);
+	}
+
+	public void oFinished(float time)
+	{
+//		hasWon
+		if(!haveFinished)
+			iWon = true;
+		//GameObject.Find("GameController").GetComponent<GameController>().FinishScreen(!haveFinished, time);
+		haveFinished = true;
+		networkView.RPC("iFinished", RPCMode.Others, new object[]{time});
+
+	}
+	
+	[RPC]	
+	void iSendscore(float score)
+	{
+		Debug.Log("iSendscore not implemented yet");
+	}
+
+	public void oSendscore()
+	{
+		Debug.Log("oSendscore not implemented yet");
+		networkView.RPC("iSendscore", RPCMode.Others, new object[]{100f});
+	}
+
+	[RPC]	
+	void iResetGyro(float duration)
+	{
+		Debug.Log("iResetGyro not implemented yet");
+	}
+
+	public void oResetGyro()
+	{
+		Debug.Log("oResetGyro not implemented yet");
+		networkView.RPC("iResetGyro", RPCMode.Others, new object[]{1f});
+	}
+
+	[RPC]	
+	void iStartGame(string levelName)
+	{
+        //LoadingScene.nextLevel = levelName;
+		Application.LoadLevel("Loading");
+		Time.timeScale = 0f;
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		if(level == 2)
+			oIsReady();
+		else if(level == 0)
+			CleanUp();
+	}
+
+	void CleanUp()
+	{
+		Network.Disconnect();
+		MasterServer.UnregisterHost();
+	}
+	
+	public void oStartGame(string levelName)
+	{
+		networkView.RPC("iStartGame", RPCMode.All, new object[]{levelName});
+	}
+
+	int isReady = 0;
+	[RPC]
+    void iIsReady()
+	{
+		isReady++;
+	}
+	
+	public void oIsReady()
+	{
+		networkView.RPC("iIsReady", RPCMode.All, null);
+	}
+	#endregion
+}
